8062efb7e7ca1ca418a82152550a2d5820ecf079
davideparisimodena <50918045+davideparisimodena@users.noreply.github.com>
2023-08-12 14:44:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,11 @@ uploaded_file = st.file_uploader("Scegli un file PDF", type="pdf")
 if uploaded_file is not None:
    with open('uploaded_file.pdf', 'wb') as f:
         f.write(uploaded_file.getbuffer())
-    pdf = PdfReader('uploaded_file.pdf')
-    text = ""
-    for page in pdf.pages:
-        text += page.extract_text()
+   pdf = PdfReader('uploaded_file.pdf')
+   text = ""
+   for page in pdf.pages:
+      text += page.extract_text()
     #pdf_to_text('uploaded_file.pdf')
-    #return text 
+    #return text
 st.write(text)
 
