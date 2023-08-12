c7ad55341e901f07659647bd386ff57b5582a760
davideparisimodena <50918045+davideparisimodena@users.noreply.github.com>
2023-08-12 14:41:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,13 @@ uploaded_file = st.file_uploader("Scegli un file PDF", type="pdf")
 
 # Se il file è caricato, leggi il contenuto
 if uploaded_file is not None:
-    # Crea un oggetto PdfFileReader dal file caricato
-    pdf_reader = PyPDF2.PdfReader(uploaded_file)
-    # Ottieni il numero di pagine del pdf
-    num_pages = len(pdf_reader.pages)
-    # Inizializza una lista vuota per contenere il testo di ogni pagina
-    pages = []
-    # Itera su ogni pagina e aggiungi il testo alla lista
-    for i in range(num_pages):
-        page = pdf_reader.pages(i)
-        text = page.extractText()
-        pages.append(text)
-    # Unisci il testo di tutte le pagine in una stringa
-    pdf_text = "\n".join(pages)
-    # Mostra il testo del pdf
-    st.write(pdf_text)
+   with open('uploaded_file.pdf', 'wb') as f:
+        f.write(uploaded_file.getbuffer())
+    pdf = PdfReader('uploaded_file.pdf')
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
+    #pdf_to_text('uploaded_file.pdf')
+    #return text 
+    st.write(text)
 
